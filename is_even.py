e43d2fde016afc37d2a0f42a32f04a9d3313ecbc
lazizjonalimov <77163755+lazizjonalimov@users.noreply.github.com>
2021-07-15 10:22:33 +0500

    Delete is_even.py

--- is_even.py
+++
@@ -1,8 +0,0 @@
-
-def isEven(num):
-  if num % 2 == 0:
-    print("The secret number is even")
-  elif num % 2 == 1:
-    print("The secret number is odd")
-  else: 
-    print("The secret number is neither odd nor even.")
